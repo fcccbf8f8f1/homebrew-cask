da4046348552ead96513dc6c6742e4a9b4a11ef1
Vítor Galvão <info@vitorgalvao.com>
2017-01-05 19:36:05 +0000

    Update script.sh

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -10,14 +10,16 @@
 
 header 'Running script.sh...'
 
-# See https://github.com/travis-ci/travis-ci/issues/4596 (still open at the time of writting this comment) for the reason to use "${TRAVIS_COMMIT_RANGE/.../..}"
-modified_casks=($(git diff --name-only --diff-filter=AM "${TRAVIS_COMMIT_RANGE/.../..}" -- Casks/*.rb))
-ruby_files_added_outside_casks_dir=($(git diff --name-only --diff-filter=A "${TRAVIS_COMMIT_RANGE/.../..}" -- *.rb))
+# See https://github.com/travis-ci/travis-ci/issues/4596 (still open at the time of writting this comment) for the reason of this workaround
+commit_range="${TRAVIS_COMMIT_RANGE/.../..}"
+
+modified_casks=($(git diff --name-only --diff-filter=AM "${commit_range}" -- Casks/*.rb))
+ruby_files_added_outside_casks_dir=($(git diff --name-only --diff-filter=A "${commit_range}" -- *.rb))
 
 if [[ ${#ruby_files_added_outside_casks_dir[@]} -gt 0 ]]; then
   odie "Casks added outside Casks directory: ${ruby_files_added_outside_casks_dir[@]}"
 elif [[ ${#modified_casks[@]} -gt 0 ]]; then
-  run brew cask _audit_modified_casks "${TRAVIS_COMMIT_RANGE/.../..}"
+  run brew cask _audit_modified_casks "${commit_range}"
   run brew cask style "${modified_casks[@]}"
 else
   ohai 'No casks modified, skipping'
