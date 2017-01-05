54ae607a7fad7df549ecf3b2a074646db73fddc1
Vítor Galvão <info@vitorgalvao.com>
2017-01-05 19:15:47 +0000

    travis: fail if any cask files added outside Casks dir

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -10,8 +10,12 @@
 
 header 'Running script.sh...'
 
-if any_casks_modified; then
-  modified_casks=($(modified_cask_files))
+modified_casks=($(git diff --name-only --diff-filter=AM "${TRAVIS_COMMIT_RANGE}" -- Casks/*.rb))
+ruby_files_added_outside_casks_dir=($(git diff --name-only --diff-filter=A "${TRAVIS_COMMIT_RANGE}" -- *.rb))
+
+if [[ ${#ruby_files_added_outside_casks_dir[@]} -gt 0 ]]; then
+  odie "Casks added outside Casks directory: ${ruby_files_added_outside_casks_dir[@]}"
+elif [[ ${#modified_casks[@]} -gt 0 ]]; then
   run brew cask _audit_modified_casks "${TRAVIS_COMMIT_RANGE}"
   run brew cask style "${modified_casks[@]}"
 else
